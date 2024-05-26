52cf4bb858c5949af893304203703b543164cdac
Thach Minh Hao <131192001+haothach30@users.noreply.github.com>
2024-05-26 16:09:19 +0700

    Delete Lap trinh Java directory

--- Lap trinh Java/Quản lý Động vật/Bo.java
+++
@@ -1,8 +0,0 @@
-package Buoi04;
-
-public class Bo extends DVat{
-	public void keu() {
-		System.out.println("Oa...oa...");
-	}
-}
-
--- Lap trinh Java/Quản lý Động vật/Meo.java
+++
@@ -1,7 +0,0 @@
-package Buoi04;
-
-public class Meo extends DVat{
-	public void keu() {
-		System.out.println("meo...meo...");
-	}
-}
